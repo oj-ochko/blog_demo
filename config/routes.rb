bf8639c881ac1043058df48ea6905656930b966d
oj-ochko <73417322+oj-ochko@users.noreply.github.com>
2022-05-21 21:28:45 +0800

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/profile'
+  devise_for :users, controllers: { 
+    sessions: 'users/sessions', 
+    registrations: 'users/registrations'
+  }
+  get '/u/:id', to: 'users#profile', as: 'user'
   resources :posts
   get 'pages/about', to: 'pages#about'
- 
+
   root "pages#home"
 end
